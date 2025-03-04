4f307338bc8178ae25e3e27f62b01e2225675e1f
Boris Damevin <borisdamevin@gmail.com>
2025-03-04 16:41:04 +0100

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const classes = {
+    tailwind: [
+        'flex',
+        'min-h-screen',
+        'h-full',
+        'w-full',
+        'justify-center'
+    ]
+};
+</script>
+
+<template>
+    <div :class="classes.tailwind">
+        <slot/>
+    </div>
+</template>
+
+<style module lang="scss">
+
+</style>
