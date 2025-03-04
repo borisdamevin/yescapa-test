d8cbaea667c184dd032605e9a2a83b5c7ca4585b
Boris Damevin <borisdamevin@gmail.com>
2025-03-04 14:41:10 +0100

    build(deps): add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
     devtools: {
         enabled: true
     },
     css: [
+        '@/assets/tailwind.css',
         '@/assets/sass/app.scss'
-    ]
+    ],
+    vite: {
+        plugins: [
+            tailwindcss()
+        ]
+    }
 });
